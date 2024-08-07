e8895e1423018d6290315831ed6a1bbc67b07c9c
full-stack-hacks <redTapeRedTape@protonmail.com>
2024-08-07 11:19:59 -0500

    Merge branch 'feature'

--- onFeature.js
+++ onFeature.js
@@ -1,1 +1,12 @@
-console.log('on feature')
+console.log("on feature");
+const addedToFeature = "From fullstackhacks";
+const addedFromAdamInFeature = "true";
+
+function add(a, b) {
+  return a + b;
+}
+
+const testFromlocal = true;
+const oneMoreThingTosSolve = true;
+
+const testingFromPull = true;
