9d8a802f9a76981c21423b0a6c487d1b06842ea8
full-stack-hacks <redTapeRedTape@protonmail.com>
2024-08-07 12:21:06 -0500

    wrote some new code

--- phaseTwo.js
+++ phaseTwo.js
@@ -1,1 +1,10 @@
-const thisIsPhaseTwo = true
+const thisIsPhaseTwo = true;
+const featureOneA = (arr) => {
+  arr.forEach((elem) => {
+    console.log(elem);
+  });
+};
+
+const arr = [1, 2, 3, 4];
+const ouput = featureOneA(arr);
+console.log(output);
