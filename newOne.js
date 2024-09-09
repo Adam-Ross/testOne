36378c44054e4dee0a5cfb8164230beae7ec1130
full-stack-hacks <redTapeRedTape@protonmail.com>
2024-09-09 13:56:07 -0500

    Added some code

---
+++ newOne.js
@@ -0,0 +1,24 @@
+const test = [
+  {
+    name: "Garrett",
+    age: "shit",
+  },
+  {
+    name: "Phil Hawke tua",
+    age: "shit",
+  },
+  {
+    name: "Mike Jones",
+    age: "shit",
+  },
+];
+
+const getNames = function (args) {
+  const names = args.map((elem) => {
+    return elem.name;
+  });
+  return names;
+};
+
+const names = getNames(test);
+console.log(names);
